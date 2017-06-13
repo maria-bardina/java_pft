dca264a7e5d0dc6477c39b1ea1e59345c0d6b234
bardina_md <bardina_md@bw-sw.com>
2017-06-13 20:34:15 +0700

    сравнение коллецкий по именам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -15,11 +15,33 @@ public class GroupData {
         return groupName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return groupName != null ? groupName.hashCode() : 0;
+    }
+
     public String getHeader() {
         return header;
     }
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "groupName='" + groupName + '\'' +
+                '}';
     }
 }

0f916971d35d43910596aa7b20efbb257cc70a0a
mdbardina <bardina_md@bw-sw.com>
2017-08-21 12:51:07 +0700

    Logger

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -19,19 +19,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+   Logger
+
     @DataProvider
     public Iterator<Object[]> validGroupsFromJson() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"));
-        String json = "";
-        String line = reader.readLine();
-        while (line !=null){
-            json+=line;
-            line=reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"))){
+            String json = "";
+            String line = reader.readLine();
+            while (line !=null){
+                json+=line;
+                line=reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+            return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
-        Gson gson = new Gson();
-        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
-        return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
-
     }
 
     @Test(dataProvider = "validGroupsFromJson")

9593ee5c8b379f3592e8c5fbaee8ec3ab85e44b8
bardina_md <bardina_md@bw-sw.com>
2017-08-25 09:04:42 +0700

    work

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTests.java
@@ -1,7 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.junit.Test;
+
 /**
  * Created by bardina_md on 22.08.17.
  */
 public class HbConnectionTests {
+    @Test
+    public void testHbConnectionTests(){
+
+    }
 }

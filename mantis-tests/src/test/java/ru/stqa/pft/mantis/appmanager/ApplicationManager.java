64e6f527839a55af4f36e9fc052ac6d04df2433d
bardina_md <bardina_md@bw-sw.com>
2017-09-03 23:05:57 +0700

    last homework

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,50 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by bardina_md on 26.07.17.
+ */
+public class ApplicationManager {
+    private Properties properties;
+    private WebDriver wd;
+
+
+    private String browser;
+    
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader((new File((String.format("src/test/resources/%s.properties", target))))));
+
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+
+}

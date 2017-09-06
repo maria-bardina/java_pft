0ec70b9c3eafd78d180ddcd4e34e3064b77c6663
mdbardina <bardina_md@bw-sw.com>
2017-09-06 15:56:16 +0700

    лекция 8,5 нужно добавить пользователя, ближе к концу лекции

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -11,17 +11,20 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by bardina_md on 26.07.17.
  */
 public class ApplicationManager {
     private Properties properties;
-    private WebDriver wd;
 
 
     private String browser;
-    
+    private WebDriver wd;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
+
     public ApplicationManager(String browser) {
         this.browser = browser;
         properties = new Properties();
@@ -31,30 +34,50 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader((new File((String.format("src/test/resources/%s.properties", target))))));
+    }
 
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
+    public void stop() {
+        if (wd != null) {
+            wd.quit();
         }
     }
 
-    public void stop() {
-        wd.quit();
+    public HttpSession newSession() {
+        return new HttpSession(this);
     }
 
-public HttpSession newSession(){
-        return new HttpSession(this);
-}
-
     public String getProperty(String key) {
-    return properties.getProperty(key);
+        return properties.getProperty(key);
     }
 
     public RegistrationHelper registration() {
-        return null;
+        if(registrationHelper==null){
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public FtpHelper ftp(){
+        if (ftp==null){
+            ftp=new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseURL"));
+        }
+        return wd;
     }
 }

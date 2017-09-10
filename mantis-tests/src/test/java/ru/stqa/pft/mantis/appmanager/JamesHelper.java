7e41e48c0a3a479061ef708e6a3105c3927eb411
bardina_md <bardina_md@bw-sw.com>
2017-09-10 23:34:43 +0700

    wip

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/JamesHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/JamesHelper.java
@@ -7,7 +7,11 @@ import javax.mail.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by marru on 09.09.2017.
@@ -23,32 +27,32 @@ public class JamesHelper {
     private Store store;
     private String mailserver;
 
-    public JamesHelper (ApplicationManager app){
+    public JamesHelper(ApplicationManager app) {
         this.app = app;
-        telnet=new TelnetClient();
-        mailSession=Session.getDefaultInstance(System.getProperties());
+        telnet = new TelnetClient();
+        mailSession = Session.getDefaultInstance(System.getProperties());
     }
 
-    public boolean doesUserExit (String name){
+    public boolean doesUserExit(String name) {
         initTelnetSession();
         write("verify" + name);
-        String result = readUntil ("exit");
+        String result = readUntil("exit");
         closeTelnetSession();
-        return result.trim().equals("User"+name+"exit");
+        return result.trim().equals("User" + name + "exit");
     }
 
-    public void createUser (String name,String passwd){
+    public void createUser(String name, String passwd) {
         initTelnetSession();
-        write("adduser"+name+" "+ passwd);
-        String result = readUntil ("User"+name+"added");
+        write("adduser " + name + " " + passwd);
+        String result = readUntil("User " + name + " added");
         closeTelnetSession();
     }
 
 
-    public void deleteUser (String name){
+    public void deleteUser(String name) {
         initTelnetSession();
         write("deluser" + name);
-        String result = readUntil ("User"+name+"deluser");
+        String result = readUntil("User" + name + "deluser");
         closeTelnetSession();
     }
 
@@ -62,36 +66,37 @@ public class JamesHelper {
         try {
             telnet.connect(mailserver, port);
             in = telnet.getInputStream();
-            out = new PrintStream(telnet.getOutputStream() );
-            }catch (Exception e){
+            out = new PrintStream(telnet.getOutputStream());
+        } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
         readUntil("Login id:");
-        write("");
-        readUntil ("Password");
+        write("root");
+        readUntil("Password");
         write(password);
 
-        readUntil ("Welcome"+login+". HELP for lost of commands");
+        readUntil("Welcome " + login + ". HELP for a list of commands");
 
     }
 
 
     private String readUntil(String pattern) {
         try {
-            char lastChar = pattern.charAt(pattern.length()-1);
+            char lastChar = pattern.charAt(pattern.length() - 1);
             StringBuffer sb = new StringBuffer();
             char ch = (char) in.read();
-            while (true){
-                System.out.println(ch);
+            while (true) {
+                System.out.print(ch);
                 sb.append(ch);
-                if (ch==lastChar){
-                    if (sb.toString().endsWith(pattern)){
+                if (ch == lastChar) {
+                    if (sb.toString().endsWith(pattern)) {
+                        System.out.println();
                         return sb.toString();
                     }
                 }
-                ch=(char) in.read();
+                ch = (char) in.read();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -99,30 +104,71 @@ public class JamesHelper {
         return null;
     }
 
-    private void write (String value){
-        try{
+    private void write(String value) {
+        try {
             out.println(value);
             out.flush();
             System.out.println(value);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void closeTelnetSession(){
+    private void closeTelnetSession() {
         write("quit");
     }
 
-    public void drainEmail (String username, String password) throws MessagingException {
-        Folder inbox = openInbox (username,password);
-        for (Message message : inbox.getMessages()){
-            message.setFlag(Flags.Flag.DELETED,true);
+    public void drainEmail(String username, String password) throws MessagingException {
+        Folder inbox = openInbox(username, password);
+        for (Message message : inbox.getMessages()) {
+            message.setFlag(Flags.Flag.DELETED, true);
         }
         closeFolder(inbox);
     }
 
-    private Folder openInbox(String username, String password) {
+    private Folder openInbox(String username, String password) throws MessagingException {
+        store = mailSession.getStore("pop3");
+        store.connect(mailserver, username, password);
+        Folder folder = store.getDefaultFolder().getFolder("INBOX");
+        folder.open(Folder.READ_WRITE);
+        return folder;
     }
+
+    public List<MailMessage> waitForMail(String username, String password, long timeout) throws MessagingException {
+        long now = System.currentTimeMillis();
+        while (System.currentTimeMillis() < now + timeout) {
+        List<MailMessage>allMail = getAllMail(username, password);
+        if (allMail.size() > 0) {
+            return allMail;
+        }
+        try {
+            {
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        }
+        throw new Error ("No mail:(");
+    }
+
+    private List<MailMessage> getAllMail(String username, String password) throws MessagingException {
+        Folder inbox = openInbox(username, password);
+        List <MailMessage>message= Stream.of(inbox.getMessages()).map(JamesHelper::toModelMail)
+                .collect(Collectors.toList());
+        closeFolder(inbox);
+        return message;
+    }
+
+    public static MailMessage toModelMail(Message m) {
+        try {
+            return new MailMessage(m.getAllRecipients()[0].toString(), (String) m.getContent());
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     private void closeFolder(Folder folder) throws MessagingException {
         folder.close(true);

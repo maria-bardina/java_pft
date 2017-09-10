24c22dfaa2e1df26df1a058eaf83ccd4d216bfba
bardina_md <bardina_md@bw-sw.com>
2017-09-10 11:04:46 +0700

    WIP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,11 +1,14 @@
 package ru.stqa.pft.mantis.tests;
 
 
+import org.testng.annotations.Test;
+
 /**
  * Created by marru on 05.09.2017.
  */
 public class ReistrationTests extends TestBase {
-    @org.testng.annotations.Test
+
+    @Test
     public void testRegistration(){
         app.registration().start("user1","mail@mail.com");
 

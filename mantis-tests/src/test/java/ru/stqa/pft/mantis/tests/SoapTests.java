3fd4181bfb544fc3970ab940319a4dbead887aaf
bardina_md <bardina_md@bw-sw.com>
2017-09-11 22:06:16 +0700

    wip

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by marru on 11.09.2017.
+ */
+public class SoapTests {
+
+    @Test
+    public void testGetProgects(){
+        new Mantis
+    }
+
+}
